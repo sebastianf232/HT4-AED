8df2dd0b6346cc5e582e4baa9ba263f3155bce72
Sebastian Franco <diego.sebas.franco@gmail.com>
2022-02-20 23:40:23 -0600

    Creacion de clases inicial

---
+++ ListasSingle.java
@@ -0,0 +1,11 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Hoja de Trabajo 4
+ * @author Juan Miguel Gonzalez-Campo 21077
+ * @author Sebastian Franco 21484
+ * @author Dimitrio Badani 20092
+ * Clase Stack Listas Single linked
+ */
+public class ListasSingle {
+}
